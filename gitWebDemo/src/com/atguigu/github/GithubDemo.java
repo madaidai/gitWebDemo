7cb9e5172b638d898ddf30ec692ec3545c38f4da
madaidai <wsrjcck10@sina.com>
2015-09-24 22:43:26 +0800

    Update GithubDemo.java
    origin update 01

--- gitWebDemo/src/com/atguigu/github/GithubDemo.java
+++ gitWebDemo/src/com/atguigu/github/GithubDemo.java
@@ -5,5 +5,8 @@ public class GithubDemo {
 	public static void main(String[] args){
 		
 		System.out.println("hello world!!");
+		for (int x = 0; x< 10;x++){
+			System.out.println("i want to play dota");
+		}
 	}
 }
